449bcee830806de0a415ca480fa706d54b8f63bb
Abdela Umer <adheremo65@gmail.com>
2023-09-18 23:53:55 -0400

    first full stack app

--- server/app.py
+++ server/app.py
@@ -16,11 +16,55 @@ db.init_app(app)
 
 @app.route('/messages')
 def messages():
-    return ''
+    messages_all = Message.query.order_by(Message.created_at.asc()).all()
+    messages_list = []
+    for each in messages_all:
+        message_dict = each.to_dict()
+        messages_list.append(message_dict)
+    response = make_response(messages_list,200)
+    return response
 
-@app.route('/messages/<int:id>')
+@app.route("/messages", methods = ["POST"])
+def posted():
+    if request.is_json:
+        data = request.get_json()
+        new_message = Message(
+            username =  data.get("username"),
+            body =  data.get("body"),
+        )
+        db.session.add(new_message)
+        db.session.commit()
+    response_body = new_message.to_dict()
+    response = make_response(response_body,201)
+    return response
+
+
+    
+
+@app.route('/messages/<int:id>',methods = ["PATCH"])
 def messages_by_id(id):
-    return ''
+    Post_by_id = Message.query.filter(Message.id == id).first()
+    if request.is_json:
+        data = request.get_json()
+        for key,value in data.items():
+            setattr(Post_by_id,key,value)
+        db.session.commit()
+        
+   
+    response_body = Post_by_id.to_dict()
+    response = make_response(response_body,201)
+    return response
+@app.route("/messages/<int:id>", methods = ["DELETE"])
+def deleted_message(id):
+    deleted_by_id = Message.query.filter(Message.id == id).first()
+    db.session.delete(deleted_by_id)
+    db.session.commit()
+
+
+
+
+        
+    
 
 if __name__ == '__main__':
     app.run(port=5555)
